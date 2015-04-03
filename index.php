fe9a19a254b50f29c26add459830900ba0c0a947
Luis <0gravity.ls@gmail.com>
2015-04-03 13:30:47 -0400

    User Authentication
    Forgot to add session_start to the index.php file.

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 		<br><br><center><div class="logo"><a href="index.php" style="text-decoration: none; color: #333333;">College Event Website</a></div></center>		
 
 		<br>
-		<?php if( isset( $_SESSION['user_id'] ) ): ?>
+		<?php session_start(); if( !isset( $_SESSION['user_id'] ) ): ?>
 		
 			<center><p class="body">
 				<h3>Login Below</h3>
