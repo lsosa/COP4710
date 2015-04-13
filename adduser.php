fbbb12fd123bb656012b51e0f058377976a7c797
Luis <0gravity.ls@gmail.com>
2015-04-13 12:17:33 -0400

    Added a footer and header files

--- adduser.php
+++ adduser.php
@@ -10,37 +10,8 @@ $form_token = md5( uniqid('auth', true) );
 $_SESSION['form_token'] = $form_token;
 ?>
 
-<!doctype html>
-<html>
-<head>
-	<title>Register</title>
-	<meta name="description" content="Project for Database Management Systems COP 4710" />
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Just+Me+Again+Down+Here" />
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Amaranth" />
-	<style>
-		#page {
-			max-width: 800px;
-			margin: 0px auto;
-			padding: 0px 40px;
-		}
-		.logo { margin-bottom: 40px; font-family: Just Me Again Down Here; font-size: 50px; font-style: normal; font-variant: normal; font-weight: 500; line-height: 26.4px; }
-		p.body { font-family: Amaranth; font-size: 20px; font-style: normal; font-variant: normal; font-weight: 300; line-height: 35px; }
-		.paragraph {
-			font-size: 17pt;
-		}
-		.footer {
-			text-align: center;
-			max-width: 646px;
-			margin: 0px auto;
-		}
-		body {
-			font-family: "open sans", sans-serif;
-			color: #333333;
-		}
-	</style>
-</head>
-
-<body>	
+<?php include_once("header.php"); ?>
+	
 	<div id="page">
 		<br><br><center><div class="logo"><a href="index.php" style="text-decoration: none; color: #333333;">College Event Website</a></div></center>		
 
@@ -75,4 +46,5 @@ $_SESSION['form_token'] = $form_token;
 		
 	</div>
 </body>
-</html>
+
+<?php include_once("footer.php"); ?>
---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+
+/*** begin our session ***/
+session_start();
+
+?>
+
+<!doctype html>
+<html>
+<head>
+	<title>College Event Website</title>
+	<meta name="description" content="Project for Database Management Systems COP 4710" />
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Just+Me+Again+Down+Here" />
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Amaranth" />
+	<style>
+		#page {
+			max-width: 800px;
+			margin: 0px auto;
+			padding: 0px 40px;
+		}
+		.logo { margin-bottom: 40px; font-family: Just Me Again Down Here; font-size: 50px; font-style: normal; font-variant: normal; font-weight: 500; line-height: 26.4px; }
+		p.body { font-family: Amaranth; font-size: 20px; font-style: normal; font-variant: normal; font-weight: 300; line-height: 35px; }
+		.paragraph {
+			font-size: 17pt;
+		}
+		.footer {
+			text-align: center;
+			max-width: 646px;
+			margin: 0px auto;
+		}
+		body {
+			font-family: "open sans", sans-serif;
+			color: #333333;
+		}
+	</style>
+</head>
+<body>
