5b1b08ad2b000ef72db91cebae71a591c55ac9f5
Michael Schnell <michaelschnell@knights.ucf.edu>
2015-04-16 11:14:45 -0400

    hostevents has link

--- index.php
+++ index.php
@@ -289,7 +289,7 @@ function listRSOEvents() {
 					</div>
 				<?php elseif (isset($_SESSION['user_priv']) && $_SESSION['user_priv'] == 2): ?>
 					<div id="navbar">
-					<h4><a href="logout.php">Logout</a> &nbsp&nbsp&nbsp <a href="/">Host Event</a></h4>
+					<h4><a href="logout.php">Logout</a> &nbsp&nbsp&nbsp <a href="HostEvents.php">Host Event</a></h4>
 					</div>
 				<?php elseif (isset($_SESSION['user_priv']) && $_SESSION['user_priv'] == 1): ?>
 					<div id="navbar">
